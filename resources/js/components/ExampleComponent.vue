96c0652ac130745a981cf4b187c8633d9e212297
ysrael-tavares <ysraeltavares33@gmail.com>
2022-09-09 00:29:47 -0300

    Update ExampleComponent.vue
    Listagem inicial dos personagens

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,30 +1,57 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Configuração de Ambiente</div>
-
-                    <div class="card-body">
-                        <input type="text" v-model="teste">
-
-                        {{teste}}
-                    </div>
-                </div>
+    <div class="d-flex justify-content-center align-items-start flex-wrap">
+        
+        <div class="card m-3 mw-250px" v-for="char in dataChar.results">
+            <img class="card-img-top" :src="char.image" alt="Card image cap">
+            <div class="card-body">
+                <h2>
+                    {{char.name}}
+                </h2>
+                <p>
+                    {{char.status}}
+                </p>
             </div>
         </div>
+
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         data: () => {
             return {
-                teste: ''
+                dataChar: []
             }
         },
         mounted() {
             console.log('Component mounted.')
+            this.start()
+        },
+        methods: {
+            async start() 
+            {
+                axios
+                .get("https://rickandmortyapi.com/api/character")
+                .then((res) => {
+                    console.log(res.data)
+                    this.dataChar = res.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            }
         }
     }
 </script>
+
+<style>
+    .mw-100px{
+        max-width: 100px;
+    }
+
+    .mw-250px{
+        max-width: 250px;
+    }
+</style>
